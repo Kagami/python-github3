d2ba1fd46dc780be19eb67f8453c80b126338a20
Kagami Hiiragi <kagami@genshiken.org>
2015-03-30 22:28:14 +0300

    Bump version to 0.6.2

--- pygithub3/__init__.py
+++ pygithub3/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 __title__ = 'pygithub3'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __author__ = 'David Medina'
 __email__ = 'davidmedina9@gmail.com'
 __license__ = 'ISC'
